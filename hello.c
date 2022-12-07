269889c7bf87e20f16c5a7b0d0c355785f1acd21
Jacob Dekaney <105133765+JacobDekaney@users.noreply.github.com>
2022-12-07 15:49:53 -0500

    Adding comments
    Added comments and descriptions

--- hello.c
+++ hello.c
@@ -1,7 +1,13 @@
+/*
+Code written for the CS50 certification course
+
+Code takes user input and prints a statement based off the user input
+*/
+
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void) {
-    string name = get_string("What's your name? ");
-    printf("Hello, %s\n", name);
+    string name = get_string("What's your name? "); //takes input from the user
+    printf("Hello, %s\n", name); //prints Hello, with the user's input
 }
--- mario.c
+++ mario.c
@@ -1,3 +1,10 @@
+/*
+Code for the CS50 cerification course
+
+This code pritns out a pyramid of #s based on the number
+inputted by the user
+*/
+
 #include <cs50.h>
 #include <stdio.h>
 
@@ -5,27 +12,25 @@ int main(void)
 {
     int level = -1;
     while (level < 0 || level > 8){
-        level = get_int("Height: ");
+        level = get_int("Height: "); //takes a user input the is between 0 and 8
     }
 
     char pyramid = '#';
     int space = level;
 
     for (int i = 0; i < level; i++){
-        for (int j = space - 1; j > 0; j--) {
+        for (int j = space - 1; j > 0; j--) { //prints the amount of spaces based on pyramid size and level
             printf(" ");
         }
-        for (int k = 0; k <= i; k++) {
+        for (int k = 0; k <= i; k++) { //prints the left side of the pyramid based on size and level
             printf("%c",pyramid);
         }
         printf("  ");
-        for (int k = 0; k <= i; k++) {
+        for (int k = 0; k <= i; k++) { //prints the right side of the pyramid based on size and level
             printf("%c",pyramid);
         }
         printf("\n");
 
-        //printf("%c  %c\n", pyramid, pyramid);
-        //pyramid = pyramid + "#";
         space--;
     }
 }
